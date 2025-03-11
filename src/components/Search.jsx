c5290ef706e8be79668d05c77d41755a52a25857
nitinsahu19 <sahunitin900@gmail.com>
2025-03-11 15:17:07 +0530

    Implemented PostListPage with SideMenu component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 const Search = () => {
   return (
-    <div className="bg-gray-100 p-2 rounded-full flex items-center gap-2">
+    <div className="bg-gray-100 p-2 rounded-full flex items-center gap-2 w-max">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
@@ -15,7 +15,7 @@ const Search = () => {
       <input
         type="text"
         placeholder="search a post..."
-        className="bg-transparent"
+        className="bg-transparent outline-none"
       />
     </div>
   );
--- src/routes/PostListPage.jsx
+++ src/routes/PostListPage.jsx
@@ -1,5 +1,36 @@
+import PostList from "../components/PostList";
+import SideMenu from "../components/SideMenu";
+import { useState } from "react";
+
 const PostListPage = () => {
-  return <div>PostListPage</div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="">
+      <h1 className="mb-8 text-2xl">Development Blog</h1>
+
+      {/* Dynamic button for filter */}
+      <button
+        onClick={() => setOpen((prev) => !prev)}
+        className="bg-blue-800 px-4 py-2 text-sm text-white rounded-2xl mb-4 md:hidden"
+      >
+        {open ? "Close" : "Filter or Search"}
+      </button>
+
+      {/* Main container */}
+      <div className="flex flex-col-reverse md:flex-row gap-8">
+        {/* Left box for posts */}
+        <div className="flex flex-col">
+          <PostList />
+        </div>
+
+        {/* Right box for options */}
+        <div className={`${open ? "block" : "hidden"} md:block`}>
+          <SideMenu />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostListPage;
