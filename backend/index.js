248136f58333e1e5e78ac8a369153f0ce3fe5cc7
nitinsahu19 <sahunitin900@gmail.com>
2025-03-15 13:37:18 +0530

    done with CRUD operation through controllers

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,21 @@ import commentRouter from "./routes/comment.route.js";
 
 const app = express();
 connectDB();
+app.use(express.json());
 
 app.use("/posts", postRouter);
 app.use("/users", userRouter);
 app.use("/comments", commentRouter);
+
+// Error handler
+app.use((error, req, res, next) => {
+  const statusCode = error.status || 500;
+
+  res.status(statusCode).json({
+    message: error.message || "Something went wrong!",
+    status: statusCode,
+    stack: process.env.NODE_ENV === "development" ? error.stack : undefined,
+  });
+});
 
 app.listen(3000, () => console.log("Server is running!"));
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,11 +1,16 @@
 import express from "express";
-import Post from "../models/post.model.js";
+import {
+  getPost,
+  getPosts,
+  createPost,
+  deletePost,
+} from "../controllers/post.controller.js";
 
 const postRouter = express.Router();
 
-postRouter.get("/", async (req, res) => {
-  const posts = await Post.find();
-  res.status(200).send(posts);
-});
+postRouter.get("/", getPosts);
+postRouter.get("/:slug", getPost);
+postRouter.post("/", createPost);
+postRouter.delete("/:id", deletePost);
 
 export default postRouter;
